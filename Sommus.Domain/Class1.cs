8548927c09dc5f55db8baa50a514184b300c89c0
Joao Lucas da Silva Ribeiro <joaolucas.jlsr@gmail.com>
2021-02-03 16:55:13 -0300

    Projeto Inicial

---
+++ Sommus.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sommus.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sommus.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sommus.Infra
+{
+    public class Class1
+    {
+    }
+}
